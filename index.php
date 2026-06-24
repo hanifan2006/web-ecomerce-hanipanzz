b70f501fb29442a1d1beefb649461b8ccc2f27b7
hanifan2006 <147569062+hanifan2006@users.noreply.github.com>
2026-06-24 13:22:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -40,28 +40,14 @@
  *---------------------------------------------------------------
  * APPLICATION ENVIRONMENT
  *---------------------------------------------------------------
- *
- * You can load different configurations depending on your
- * current environment. Setting the environment also influences
- * things like logging and error reporting.
- *
- * This can be set to anything, but default usage is:
- *
- *     development
- *     testing
- *     production
- *
- * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	// MODIFIKASI: Deteksi environment otomatis untuk Vercel
+	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : (isset($_SERVER['VERCEL']) ? 'production' : 'development'));
 
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
  *---------------------------------------------------------------
- *
- * Different environments will require different levels of error reporting.
- * By default development will show errors but testing and live will hide them.
  */
 switch (ENVIRONMENT)
 {
@@ -86,104 +72,45 @@ switch (ENVIRONMENT)
 	default:
 		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
 		echo 'The application environment is not set correctly.';
-		exit(1); // EXIT_ERROR
+		exit(1);
 }
 
 /*
  *---------------------------------------------------------------
  * SYSTEM DIRECTORY NAME
  *---------------------------------------------------------------
- *
- * This variable must contain the name of your "system" directory.
- * Set the path if it is not in the same directory as this file.
  */
 $system_path = __DIR__ . '/system';
+
 /*
  *---------------------------------------------------------------
  * APPLICATION DIRECTORY NAME
  *---------------------------------------------------------------
- *
- * If you want this front controller to use a different "application"
- * directory than the default one you can set its name here. The directory
- * can also be renamed or relocated anywhere on your server. If you do,
- * use an absolute (full) server path.
- * For more info please see the user guide:
- *
- * https://codeigniter.com/userguide3/general/managing_apps.html
- *
- * NO TRAILING SLASH!
  */
 $application_folder = __DIR__ . '/application';
 
-// Deteksi environment secara otomatis
-if (isset($_SERVER['VERCEL_ENV']) || isset($_SERVER['VERCEL']) || isset($_ENV['VERCEL'])) {
-    // Di Vercel, path tetap sama
-    $system_path = __DIR__ . '/system';
-    $application_folder = __DIR__ . '/application';
-}
 /*
  *---------------------------------------------------------------
  * VIEW DIRECTORY NAME
  *---------------------------------------------------------------
- *
- * If you want to move the view directory out of the application
- * directory, set the path to it here. The directory can be renamed
- * and relocated anywhere on your server. If blank, it will default
- * to the standard location inside your application directory.
- * If you do move this, use an absolute (full) server path.
- *
- * NO TRAILING SLASH!
  */
-	$view_folder = '';
-
+$view_folder = '';
 
 /*
  * --------------------------------------------------------------------
  * DEFAULT CONTROLLER
  * --------------------------------------------------------------------
- *
- * Normally you will set your default controller in the routes.php file.
- * You can, however, force a custom routing by hard-coding a
- * specific controller class/function here. For most applications, you
- * WILL NOT set your routing here, but it's an option for those
- * special instances where you might want to override the standard
- * routing in a specific front controller that shares a common CI installation.
- *
- * IMPORTANT: If you set the routing here, NO OTHER controller will be
- * callable. In essence, this preference limits your application to ONE
- * specific controller. Leave the function name blank if you need
- * to call functions dynamically via the URI.
- *
- * Un-comment the $routing array below to use this feature
  */
-	// The directory name, relative to the "controllers" directory.  Leave blank
-	// if your controller is not in a sub-directory within the "controllers" one
 	// $routing['directory'] = '';
-
-	// The controller class file name.  Example:  mycontroller
 	// $routing['controller'] = '';
-
-	// The controller function you wish to be called.
 	// $routing['function']	= '';
-
 
 /*
  * -------------------------------------------------------------------
  *  CUSTOM CONFIG VALUES
  * -------------------------------------------------------------------
- *
- * The $assign_to_config array below will be passed dynamically to the
- * config class when initialized. This allows you to set custom config
- * items or override any default config values found in the config.php file.
- * This can be handy as it permits you to share one application between
- * multiple front controller files, with each file containing different
- * config values.
- *
- * Un-comment the $assign_to_config array below to use this feature
  */
 	// $assign_to_config['name_of_config_item'] = 'value of config item';
-
-
 
 // --------------------------------------------------------------------
 // END OF USER CONFIGURABLE SETTINGS.  DO NOT EDIT BELOW THIS LINE
@@ -194,8 +121,6 @@ if (isset($_SERVER['VERCEL_ENV']) || isset($_SERVER['VERCEL']) || isset($_ENV['V
  *  Resolve the system path for increased reliability
  * ---------------------------------------------------------------
  */
-
-	// Set the current directory correctly for CLI requests
 	if (defined('STDIN'))
 	{
 		chdir(dirname(__FILE__));
@@ -207,7 +132,6 @@ if (isset($_SERVER['VERCEL_ENV']) || isset($_SERVER['VERCEL']) || isset($_ENV['V
 	}
 	else
 	{
-		// Ensure there's a trailing slash
 		$system_path = strtr(
 			rtrim($system_path, '/\\'),
 			'/\\',
@@ -215,12 +139,11 @@ if (isset($_SERVER['VERCEL_ENV']) || isset($_SERVER['VERCEL']) || isset($_ENV['V
 		).DIRECTORY_SEPARATOR;
 	}
 
-	// Is the system path correct?
 	if ( ! is_dir($system_path))
 	{
 		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
 		echo 'Your system folder path does not appear to be set correctly. Please open the following file and correct this: '.pathinfo(__FILE__, PATHINFO_BASENAME);
-		exit(3); // EXIT_CONFIG
+		exit(3);
 	}
 
 /*
@@ -228,19 +151,11 @@ if (isset($_SERVER['VERCEL_ENV']) || isset($_SERVER['VERCEL']) || isset($_ENV['V
  *  Now that we know the path, set the main path constants
  * -------------------------------------------------------------------
  */
-	// The name of THIS file
 	define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
-
-	// Path to the system directory
 	define('BASEPATH', $system_path);
-
-	// Path to the front controller (this file) directory
 	define('FCPATH', dirname(__FILE__).DIRECTORY_SEPARATOR);
-
-	// Name of the "system" directory
 	define('SYSDIR', basename(BASEPATH));
 
-	// The path to the "application" directory
 	if (is_dir($application_folder))
 	{
 		if (($_temp = realpath($application_folder)) !== FALSE)
@@ -268,12 +183,11 @@ if (isset($_SERVER['VERCEL_ENV']) || isset($_SERVER['VERCEL']) || isset($_ENV['V
 	{
 		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
 		echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
-		exit(3); // EXIT_CONFIG
+		exit(3);
 	}
 
 	define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
 
-	// The path to the "views" directory
 	if ( ! isset($view_folder[0]) && is_dir(APPPATH.'views'.DIRECTORY_SEPARATOR))
 	{
 		$view_folder = APPPATH.'views';
@@ -305,7 +219,7 @@ if (isset($_SERVER['VERCEL_ENV']) || isset($_SERVER['VERCEL']) || isset($_ENV['V
 	{
 		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
 		echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
-		exit(3); // EXIT_CONFIG
+		exit(3);
 	}
 
 	define('VIEWPATH', $view_folder.DIRECTORY_SEPARATOR);
@@ -314,7 +228,5 @@ if (isset($_SERVER['VERCEL_ENV']) || isset($_SERVER['VERCEL']) || isset($_ENV['V
  * --------------------------------------------------------------------
  * LOAD THE BOOTSTRAP FILE
  * --------------------------------------------------------------------
- *
- * And away we go...
  */
 require_once BASEPATH.'core/CodeIgniter.php';
